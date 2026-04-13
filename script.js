26574aff844164e357968048f8b584dadefb1c21
achustephen <achustephen182@gmail.com>
2026-04-13 10:24:34 +0530

    portfolio website

---
+++ script.js
@@ -0,0 +1,20 @@
+const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// 1. Explore box model & how values are written for margin, and padding, in CSS.
+// 2. What units can be used with window.scrollY?
+// 3. Why use sticky positioning for the navbar? Why not use fixed positioning?
+// 4. How can you optimize the scroll event listener for better performance? - Moderate level
+// 5. What does transition property do in cs
+// 6. What does mx-auto do in CSS? - Easy level
+// 7. What does justify-content: center do in CSS? - Easy level
+// 8. What does align-items: center do in CSS? - Easy level
+// 9. What does text-decoration: none do in CSS? - Easy level
+// 10. what are :hover and :active in CSS? - Easy level
+// 11. How does media query work in CSS? - Moderate level
